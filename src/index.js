f4494a865823b09c6a233f20698bfd028de69d98
André <mazoni.andre@gmail.com>
2018-06-08 03:53:00 -0300

    adds payWithToken method

--- src/index.js
+++ src/index.js
@@ -23,13 +23,37 @@ module.exports = (config) => {
   });
 
   return {
-    tokenizeCard: card => postInstance.post('/1/card', {
-      CustomerName: card.customerName,
-      CardNumber: card.number,
-      Holder: card.holder,
-      ExpirationDate: card.expirationDate,
-      Brand: card.brand,
-    }),
-    getCardData: token => getInstance.get(`/1/card/${token}`),
+    cards: {
+      tokenizeCard: card => postInstance.post('/1/card', {
+        CustomerName: card.customerName,
+        CardNumber: card.number,
+        Holder: card.holder,
+        ExpirationDate: card.expirationDate,
+        Brand: card.brand,
+      }),
+      getCard: token => getInstance.get(`/1/card/${token}`),
+    },
+    creditCards: {
+      payWithToken: params => postInstance.post('/1/sales', {
+        RequestId: params.requestId,
+        MerchantOrderId: params.merchantOrderId,
+        Customer: {
+          Name: params.customerName,
+          Status: params.customerStatus,
+        },
+        Payment: {
+          Type: 'CreditCard',
+          Amount: params.amount,
+          Installments: params.installments,
+          SoftDescriptor: params.softDescriptor,
+          ReturnUrl: params.returnUrl,
+          CreditCard: {
+            CardToken: params.cardToken,
+            SecurityCode: params.cvv,
+            Brand: params.brand,
+          },
+        },
+      }),
+    },
   };
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -22,36 +22,81 @@ describe('Cielo API Wrapper', () => {
       cielo = factory(params);
     });
 
-    it('tokenizeCard: makes a POST request to /1/card passing correct parameters', () => {
-      const card = {
-        customerName: 'Biro de Biro',
-        number: '4123555598761234',
-        holder: 'BIRO BIRO',
-        expirationDate: '07/2027',
-        brand: 'Visa',
-      };
+    describe('cielo.cards', () => {
+      it('tokenizeCard: makes a POST request to /1/card passing correct parameters', () => {
+        const card = {
+          customerName: 'Biro de Biro',
+          number: '4123555598761234',
+          holder: 'BIRO BIRO',
+          expirationDate: '07/2027',
+          brand: 'Visa',
+        };
 
-      nock('https://apisandbox.cieloecommerce.cielo.com.br')
-        .post('/1/card', {
-          CustomerName: card.customerName,
-          CardNumber: card.number,
-          Holder: card.holder,
-          ExpirationDate: card.expirationDate,
-          Brand: card.brand,
-        })
-        .reply(200);
+        nock('https://apisandbox.cieloecommerce.cielo.com.br')
+          .post('/1/card', {
+            CustomerName: card.customerName,
+            CardNumber: card.number,
+            Holder: card.holder,
+            ExpirationDate: card.expirationDate,
+            Brand: card.brand,
+          })
+          .reply(200);
 
-      return expect(cielo.tokenizeCard(card)).resolves.toBeDefined();
+        return expect(cielo.cards.tokenizeCard(card)).resolves.toBeDefined();
+      });
+
+      it('getCard: makes a GET request to /1/card passing token', () => {
+        const token = '0e310ad0-622e-4374-92bd-3ed17f1d6709';
+
+        nock('https://apiquerysandbox.cieloecommerce.cielo.com.br')
+          .get(`/1/card/${token}`)
+          .reply(200);
+
+        return expect(cielo.cards.getCard(token)).resolves.toBeDefined();
+      });
     });
 
-    it('getCardData: makes a GET request to /1/card passing token', () => {
-      const token = '0e310ad0-622e-4374-92bd-3ed17f1d6709';
+    describe('cielo.creditCard', () => {
+      it('payWithToken: calls /1/sales to make a purchase with a tokenized card', () => {
+        const params = {
+          requestId: 'some_id',
+          merchantOrderId: '1234',
+          customerName: 'Biro da Silva',
+          customerStatus: 'NEW',
+          amount: 12000,
+          installments: 2,
+          softDescriptor: 'some bullcrap',
+          returnUrl: 'http://google.com',
+          cardToken: '0e310ad0-622e-4374-92bd-3ed17f1d6709',
+          cvv: '456',
+          brand: 'Master'
+        };
 
-      nock('https://apiquerysandbox.cieloecommerce.cielo.com.br')
-        .get(`/1/card/${token}`)
-        .reply(200);
+        nock('https://apisandbox.cieloecommerce.cielo.com.br')
+          .post('/1/sales', {
+            RequestId: params.requestId,
+            MerchantOrderId: params.merchantOrderId,
+            Customer: {
+              Name: params.customerName,
+              Status: params.customerStatus,
+            },
+            Payment: {
+              Type: 'CreditCard',
+              Amount: params.amount,
+              Installments: params.installments,
+              SoftDescriptor: params.softDescriptor,
+              ReturnUrl: params.returnUrl,
+              CreditCard: {
+                CardToken: params.cardToken,
+                SecurityCode: params.cvv,
+                Brand: params.brand,
+              },
+            },
+          })
+          .reply(200);
 
-      return expect(cielo.getCardData(token)).resolves.toBeDefined();
+        return expect(cielo.creditCards.payWithToken(params)).resolves.toBeDefined();
+      });
     });
   });
 });
